c0f98933bace5d6b3369e31191544897008c44fe
Henry Einhaus <heinhaus01@gmail.com>
2023-11-07 16:03:36 -0700

    Added hashtable class

---
+++ HashTable.java
@@ -0,0 +1,83 @@
+/*
+ * File: HashTable.java
+ * Author: Henry Einhaus
+ * Purpose: Simple hashtable to compare
+ * against Trie
+ */
+
+public class HashTable {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final double LOAD_FACTOR = 0.75;
+
+    private String[] table;
+    private int size;
+
+    //HashTable Constructor
+    public HashTable() {
+        table = new String[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    // hash(key) computes the hash given a key
+    private int hash(String key) {
+        // Hash: Uses java's hashcode function to compute
+        return key.hashCode() % table.length;
+    }
+
+    /*
+     * resizeTable() will resize the
+     * hashtable if the load factor is > .75
+     */
+    private void resizeTable() {
+        int newCapacity = table.length * 2;
+        String[] newTable = new String[newCapacity];
+
+        for (String key : table) {
+            if (key != null) {
+                int index = hash(key) % newCapacity;
+                while (newTable[index] != null) {
+                    index = (index + 1) % newCapacity;
+                }
+                newTable[index] = key;
+            }
+        }
+
+        table = newTable;
+    }
+
+    /*
+     * add(key) adds a string to the hashtable
+     * and will resize if neccessary.
+     * Runtime: O(1)
+     */
+    public void add(String key) {
+        if (size >= LOAD_FACTOR * table.length) {
+            resizeTable();
+        }
+
+        int index = hash(key);
+        while (table[index] != null) {
+            index = (index + 1) % table.length;
+        }
+        table[index] = key;
+        size++;
+    }
+
+    /*
+     * contains(key) is given a string and 
+     * attempts to find it in the hashtable.
+     * Returns: boolean; whether the search was successful
+     * Runtime: O(1)
+     */
+    public boolean contains(String key) {
+        int index = hash(key);
+        while (table[index] != null) {
+            if (table[index].equals(key)) {
+                return true;
+            }
+            index = (index + 1) % table.length;
+        }
+        return false;
+    }
+}
+
---
+++ HashvsTrie.java
@@ -0,0 +1,8 @@
+public class HashvsTrie {
+    
+    public static void main(String[] args) {
+        HashTable hash = new HashTable();
+        Trie trie = new Trie();
+        //TODO: Test Trie vs HashTable runtime
+    }
+}
