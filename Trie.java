ee2cc98360dbfdb0403d5e76770102ba1ab2c87e
Henry Einhaus <heinhaus01@gmail.com>
2023-11-07 14:46:41 -0700

    Added Trie info comments

--- Trie.java
+++ Trie.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
+
 class TrieNode {
     TrieNode[] children;
     boolean isEndOfWord;
 
+    // Individual Node constructer
     public TrieNode() {
         this.children = new TrieNode[26]; // Assuming the characters are in lowercase English letters
         this.isEndOfWord = false;
@@ -12,11 +14,17 @@ class TrieNode {
 
 public class Trie {
     private TrieNode root;
-
+    // Trie constructer
     public Trie() {
         this.root = new TrieNode();
     }
 
+    /*
+     * insert(word) takes a string and inserts it
+     * into the Trie while maintaining alphabetical
+     * order.
+     * Runtime: O(n)
+     */
     public void insert(String word) {
         TrieNode node = root;
         for (int i = 0; i < word.length(); i++) {
@@ -30,6 +38,12 @@ public class Trie {
         node.isEndOfWord = true;
     }
 
+    /*
+     * search(word) takes a string and searches the
+     * Trie to see if the word is inside the Trie.
+     * Returns: boolean; whether the seach was successful
+     * Runtime: O(n)
+     */
     public boolean search(String word) {
         TrieNode node = root;
         for (int i = 0; i < word.length(); i++) {
@@ -43,6 +57,12 @@ public class Trie {
         return node != null && node.isEndOfWord;
     }
 
+    /*
+     * startsWith(prefix) is given a small string
+     * and checks if it is inside of the Trie.
+     * Returns: boolean; whether the seach was successful
+     * Runtime: O(n)
+     */
     public boolean startsWith(String prefix) {
         TrieNode node = root;
         for (int i = 0; i < prefix.length(); i++) {
@@ -55,6 +75,13 @@ public class Trie {
         }
         return node != null;
     }
+
+    /*
+     * findWordsWithPrefix(prefix) finds all words
+     * in the Trie that match the given prexic.
+     * Returns; List<String>; words with given prefix.
+     * Runtime: TODO
+     */
     public List<String> findWordsWithPrefix(String prefix) {
         List<String> words = new ArrayList<>();
         TrieNode node = findNodeWithPrefix(prefix);
@@ -64,6 +91,13 @@ public class Trie {
         return words;
     }
 
+    /*
+     * findNodeWithPrefix(prefix) finds a node that
+     * matches the given prefix. Used to help funciton
+     * findWordsWithPrefix(prefix).
+     * Returns: TrieNode, A node that matches the prefix
+     * Runtime: O(n)
+     */
     private TrieNode findNodeWithPrefix(String prefix) {
         TrieNode node = root;
         for (int i = 0; i < prefix.length(); i++) {
@@ -77,6 +111,13 @@ public class Trie {
         return node;
     }
 
+    /*
+     * findWords(node,currentWord,words) is given a starting point via
+     * a node, and a string to build off of. It finds all the words in 
+     * the Trie that start with currentWord. words is updated every time
+     * another valid word is found.
+     * Runtime: TODO
+     */
     private void findWords(TrieNode node, String currentWord, List<String> words) {
         if (node.isEndOfWord) {
             words.add(currentWord);
